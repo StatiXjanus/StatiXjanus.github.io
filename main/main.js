07d6473366cd0643a68417ce25d6da0880bf141c
StatiXjanus <132628087+StatiXjanus@users.noreply.github.com>
2024-04-27 16:55:07 -0500

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,26 +1,12 @@
 var hangman = document.getElementById('hangman');
 var tic_tac = document.getElementById('tic-tac-toe');
 var block_evade = document.getElementById('block-evade');
-var mark1 = document.getElementById('block1');
-
-
-//---------EXIT-------------//
-
-mark1.addEventListener('click', ()=> {
-    window.close()
-})
-
-
-//--------------------------
-
-
-
+var solar_system = document.getElementById('solar-system');
+var earth = document.getElementById('earth');
 //---------------------------------------------
 hangman.addEventListener('mouseover', () => {
     hangman.style.background = 'red';
     hangman.style.color = 'purple';
-
-
 });
 
 
@@ -54,8 +40,6 @@ tic_tac.addEventListener('mouseout', () => {
 block_evade.addEventListener('mouseover', () => {
     block_evade.style.background = 'red';
     block_evade.style.color = 'purple';
-
-
 });
 
 
@@ -64,12 +48,46 @@ block_evade.addEventListener('mouseout', () => {
     block_evade.style.color = 'black';
 });
 
-/* ----------------------------------------------
-Links
-------------------------------------------------*/
+
+solar_system.addEventListener('mouseover', () => {
+    solar_system.style.background = 'red';
+    solar_system.style.color = 'purple';
+});
+
+
+solar_system.addEventListener('mouseout', () => {
+    solar_system.style.background = 'darkblue';
+    solar_system.style.color = 'black';
+});
+
+earth.addEventListener('mouseover', () => {
+    earth.style.background = 'red';
+    earth.style.color = 'purple';
+});
+
+
+earth.addEventListener('mouseout', () => {
+    earth.style.background = 'darkcyan';
+    earth.style.color = 'black';
+});
 
 
 
+//OPEN/LINK
+hangman.addEventListener('click', () => {
+    window.open('http://127.0.0.1:5500/games/Hangman/main.html');
+})
 
+block_evade.addEventListener('click', () => {
+    window.open('http://127.0.0.1:5500/games/block-evade/index.html');
+})
+
+solar_system.addEventListener('click', () => {
+    window.open('http://127.0.0.1:5500/space/SolarSystem/index.html');
+})
+
+earth.addEventListener('click', () => {
+    window.open('http://127.0.0.1:5500/space/Earth-sun/index.html');
+})
 
 
